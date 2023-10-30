8ededc214468d3253a18b32c0b8164c6367e8513
Arthur Duarte <103082913+aduartee@users.noreply.github.com>
2023-10-30 12:14:13 -0300

    Delete conecta.php

--- conecta.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = "root";
-$password = "";
-try {
-	$conn = new PDO('mysql:host=localhost;dbname=sistema_liberato', $username, $password);
-} catch (PDOException $e) {
-	echo "Erro na conexão" . $e->getMessage();
-}
